56ad42593f40b6bbf3467d634824103dd0c6f38c
Leovair <Leovair@users.noreply.github.com>
2019-01-09 22:20:35 +0200

    Problems of list

--- List-1.py
+++ List-1.py
@@ -1,54 +1,57 @@
-# def first_last6(nums):
-#     '''
-#     Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more.
-#     :param nums:
-#     :return:
-#     '''
-#     if nums[0] == 6 or nums[-1] == 6:
-#         return True
-#     else:
-#         return False
-#
-#
-# print(first_last6([6, 1, 2, 3]))
-#
-#
-# def same_first_last(nums):
-#     '''
-#     Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
-#     :param nums:
-#     :return:
-#     '''
-#     if len(nums) >= 1 and nums[0] == nums[-1]:
-#         return True
-#     else:
-#         return False
-#
-#
-# print(same_first_last([1, 2, 3, 1]))
-#
-# def make_pi():
-#     '''
-#     Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
-#     :return:
-#     '''
-#     return [3, 1, 4]
-#
-# def common_end(a, b):
-#     '''
-#     Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element.
-#     Both arrays will be length 1 or more.
-#     :param a:
-#     :param b:
-#     :return:
-#     '''
-#     if a[0] == b[0] or a[-1] == b[-1]:
-#         return True
-#     else:
-#         return False
-#
-#
-# common_end([1, 2, 3], [7, 3])
+def first_last6(nums):
+    '''
+    Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+    :param nums:
+    :return:
+    '''
+    if nums[0] == 6 or nums[-1] == 6:
+        return True
+    else:
+        return False
+
+
+print(first_last6([6, 1, 2, 3]))
+
+
+def same_first_last(nums):
+    '''
+    Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+    :param nums:
+    :return:
+    '''
+    if len(nums) >= 1 and nums[0] == nums[-1]:
+        return True
+    else:
+        return False
+
+
+print(same_first_last([1, 2, 3, 1]))
+
+
+def make_pi():
+    '''
+    Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+    :return:
+    '''
+    return [3, 1, 4]
+
+
+def common_end(a, b):
+    '''
+    Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element.
+    Both arrays will be length 1 or more.
+    :param a:
+    :param b:
+    :return:
+    '''
+    if a[0] == b[0] or a[-1] == b[-1]:
+        return True
+    else:
+        return False
+
+
+common_end([1, 2, 3], [7, 3])
+
 
 def sum3(nums):
     '''
@@ -76,6 +79,7 @@ def rotate_left3(nums):
     else:
         return False
 
+
 rotate_left3([5, 11, 9])
 
 
@@ -90,6 +94,7 @@ def reverse3(nums):
     else:
         return False
 
+
 print(reverse3([5, 11, 9]))
 
 
@@ -103,6 +108,7 @@ def max_end3(nums):
     if nums[0] > nums[2]:
         return [nums[0], nums[0], nums[0]]
     return [nums[2], nums[2], nums[2]]
+
 
 max_end3([11, 5, 9])
 
