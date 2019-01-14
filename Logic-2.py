94929687b4bca33af9874633b35d629f648bf790
Leovair <Leovair@users.noreply.github.com>
2019-01-14 21:58:34 +0200

    Problems of logic-2 CodingBat

--- Logic-2.py
+++ Logic-2.py
@@ -47,3 +47,29 @@ def lucky_sum(a, b, c):
     :param c:
     :return:
     '''
+    if a == 13:
+        return 0
+    elif b == 13:
+        return a
+    elif c == 13:
+        return a+b
+    else:
+      return a+b+c
+
+def fix_teen(n):
+    # if 13 <= n <= 14 or 17 <= n <= 19:
+    if n in [13, 14, 17, 18, 19]:
+        return 0
+    return n
+
+def no_teen_sum(a, b, c):
+    # Given 3 int values, a b c, return their sum. However, if any of the values
+    # is a teen -- in the range 13..19 inclusive -- then that value counts as 0,
+    # except 15 and 16 do not count as a teens. Write a separate helper "def
+    # fix_teen(n):"that takes in an int value and returns that value fixed for the
+    # teen rule. In this way, you avoid repeating the teen code 3 times (i.e.
+    # "decomposition"). Define the helper below and at the same indent level as
+    # the main no_teen_sum().
+    return fix_teen(a) + fix_teen(b) + fix_teen(c)
+
+
