2cfe8f354ce0533d8bffc307c037982c88c00ffb
Leovair <Leovair@users.noreply.github.com>
2019-01-18 20:33:31 +0200

    Problems of strings-2 CodingBat

--- String-2.py
+++ String-2.py
@@ -75,5 +75,20 @@ def end_other(a, b):
   '''
   a = a.lower()
   b = b.lower()
-  #return (b.endswith(a) or a.endswith(b))
   return a[-(len(b)):] == b or a == b[-(len(a)):]
+
+
+def xyz_there(str):
+    '''
+    Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+    :param str:
+    :return:
+    '''
+    if str[:3] == "xyz":
+        return True
+
+    for i in range(1, len(str) - 2):
+        if str[i - 1] != "." and str[i:i + 3] == "xyz":
+            return True
+
+    return False
