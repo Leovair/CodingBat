a56bc4f309e26d2b0142b730ec6beb7879b170ec
Leovair <Leovair@users.noreply.github.com>
2019-01-30 20:44:35 +0200

    Problems of LIST-2 CodingBat

--- List-2.py
+++ List-2.py
@@ -82,7 +82,7 @@ def sum67(nums):
 def has22(nums):
   """
   Given an array of ints, return True if the array contains
-  a 2 next to a 2 somewhere. Hello!
+  a 2 next to a 2 somewhere.
   """
   for i,v in enumerate(nums[:-1]):
     if v == 2 and nums[i+1] == 2:
